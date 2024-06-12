97d781f157cd3c68647b5b5e03955c27822b83d2
Bowen Gao <billgao0111@gmail.com>
2024-06-12 16:04:38 +0800

    rm

--- DrugCLIP/unimol/__init__.py
+++
@@ -1,6 +0,0 @@
-import importlib
-import unimol.tasks
-import unimol.data
-import unimol.models
-import unimol.losses
-import unimol.utils
--- DrugCLIP/unimol/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .drugclip import BindingAffinityModel
-from .transformer_encoder_with_pair import TransformerEncoderWithPair
-from .unimol import UniMolModel
