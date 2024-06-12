e3e809665082a0a5bd28a5eb0dbbf638d2542d0e
Bowen Gao <billgao0111@gmail.com>
2024-06-12 15:45:59 +0800

    code

--- DrugCLIP/py_scripts/deal_dude_lbvs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-root_path = "/mnt/nfs-ssd/data/DUD-E/"
-
-targets = os.listdir(root_path)
-print(targets)
-# remove "aa2ar"
-targets.remove("aa2ar")
--- DrugCLIP/test_sbdd.sh
+++ DrugCLIP/test_sbdd.sh
@@ -4,13 +4,23 @@ weight_path="checkpoint_best.pt"
 MOL_PATH="mols.lmdb" # path to the molecule file
 POCKET_PATH="pocket.lmdb" # path to the pocket file
 EMB_DIR="./data/emb" # path to the cached mol embedding file
+
 weight_path="/data/protein/save_dir/affinity/2023-05-06_22-08-56/checkpoint_best.pt"
 
 finetune_mol_model="/data/protein/molecule/pretrain/mol_pre_no_h_220816.pt"
 finetune_pocket_model="/data/protein/molecule/pretrain/pocket_pre_220816.pt"
 
 
-CUDA_VISIBLE_DEVICES="7" python ./unimol/encode.py --user-dir ./unimol $data_path "./data" --valid-subset test \
+encoder="drugclip" # select from [unimol, drugclip]
+
+metric="vs" # select from [sim, vs, score]
+
+test_path="/data/protein/lib-pcba/raw/lit_pcba/" # should be set to dude or pcba path
+
+model="/drug/sbdd_bench/p2m_pcba/"
+
+
+CUDA_VISIBLE_DEVICES="7" python ./unimol/test_sbdd.py --user-dir ./unimol $data_path "./data" --valid-subset test \
        --results-path $results_path \
        --num-workers 8 --ddp-backend=c10d --batch-size $batch_size \
        --task drugclip --loss in_batch_softmax --arch drugclip  \
@@ -20,4 +30,9 @@ CUDA_VISIBLE_DEVICES="7" python ./unimol/encode.py --user-dir ./unimol $data_pat
        --finetune-pocket-model $finetune_pocket_model \
        --finetune-mol-model $finetune_mol_model \
        --path $weight_path \
+       --encoder $encoder \
+       --metric $metric \
+       --testset $test_path \
+       --model $model 
+
        
