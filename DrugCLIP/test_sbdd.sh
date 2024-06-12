094a5bcf72d34d1c81452b2aa51f5750b869637f
Bowen Gao <billgao0111@gmail.com>
2024-06-12 15:50:34 +0800

    code

--- DrugCLIP/test_sbdd.sh
+++ DrugCLIP/test_sbdd.sh
@@ -1,23 +1,20 @@
 results_path="./test"  # replace to your results path
 batch_size=8
-weight_path="checkpoint_best.pt"
-MOL_PATH="mols.lmdb" # path to the molecule file
-POCKET_PATH="pocket.lmdb" # path to the pocket file
-EMB_DIR="./data/emb" # path to the cached mol embedding file
 
-weight_path="/data/protein/save_dir/affinity/2023-05-06_22-08-56/checkpoint_best.pt"
 
-finetune_mol_model="/data/protein/molecule/pretrain/mol_pre_no_h_220816.pt"
-finetune_pocket_model="/data/protein/molecule/pretrain/pocket_pre_220816.pt"
+weight_path="./checkpoints/drugclip.pt"
+
+finetune_mol_model="./checkpoints/mol_pre_no_h_220816.pt"
+finetune_pocket_model="./checkpoints/pocket_pre_220816.pt"
 
 
 encoder="drugclip" # select from [unimol, drugclip]
 
 metric="vs" # select from [sim, vs, score]
 
-test_path="/data/protein/lib-pcba/raw/lit_pcba/" # should be set to dude or pcba path
+test_path="/path/to/dude/or/lit-pcba" # should be set to dude or pcba path
 
-model="/drug/sbdd_bench/p2m_pcba/"
+model="/path/to/generated/mols"
 
 
 CUDA_VISIBLE_DEVICES="7" python ./unimol/test_sbdd.py --user-dir ./unimol $data_path "./data" --valid-subset test \
